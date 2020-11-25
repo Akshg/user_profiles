c1e3eea657cc68b94bdcb19aaf837e18206b48fa
Akash Gupta <akashsjhs@gmail.com>
2020-11-25 17:44:50 +0530

    Redirect according to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def index
+    return redirect_to new_user_session_path if current_user.nil?
+    render html: "<h1>Hii! #{current_user.name}</h1>".html_safe
+  end
+
   protected
 
   def configure_permitted_parameters
