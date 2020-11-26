868509d930db92cf94c204c92dd158b1c7d04549
Akash Gupta <akashsjhs@gmail.com>
2020-11-26 19:53:32 +0530

    Fix image issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get :profile, to: "users#show"
   resources :users, only: [:index, :edit, :update]
   root "application#index"
-  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
